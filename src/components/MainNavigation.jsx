6602785eafea7eef772497a30cf2c85343a7b710
Kronk21 <CesarAxel001@gmail.com>
2023-04-22 22:42:21 -0600

    Responsive navbar completed

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 
 const MainNavigation = function () {
+    const [navbarIsShowing, setNavbarIsShowing] = useState(false);
+
     return (
-        <header>
+        <header className={`${navbarIsShowing ? "show" : ""}`}>
             <div className="container">
                 <Link to="/" className="logo">
                     Cs<span>.dev</span>
@@ -20,6 +23,15 @@ const MainNavigation = function () {
                         <a className="navbar__link">Contacto</a>
                     </li>
                 </ul>
+
+                <button
+                    className="btn-menu"
+                    onClick={() => {
+                        setNavbarIsShowing((previousState) => !previousState);
+                    }}
+                >
+                    <i className="fa-solid fa-bars"></i>
+                </button>
             </div>
         </header>
     );
